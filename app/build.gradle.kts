6a7062e7ddc7cc4aa3b9b45ee80fce6206048885
hernan9518 <hperez1895@gmail.com>
2026-09-23 18:06:06 -0500

    feat: Configuración inicial de Room con KSP y entidad Tarea

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -36,6 +37,10 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
---
+++ app/src/main/java/com/example/persistencia/data/Tarea.kt
@@ -0,0 +1,14 @@
+package com.example.persistencia.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tabla_tareas")
+data class Tarea(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val titulo: String,
+    val descripcion: String,
+    val estadoCompletado: Boolean = false,
+    val fechaCreacion: String
+)
